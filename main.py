47f8bef146bc098f68b761579c66e41a49c74a27
Kile <k1ds1nth3dark@gmail.com>
2024-12-28 23:31:07 +0100

    Make port dependant on environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import all_routes
+from os import environ
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ for route in all_routes:
     app.register_blueprint(route)
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    app.run(port=int(environ["PORT"]), debug=True)
